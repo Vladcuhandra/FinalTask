edd5a50f4621f22f6d057733211042035d3353ae
tdlschool <tdlschool@tdlschool-10205.local>
2023-08-23 15:18:10 +0300

    second

--- src/test/java/AutomationTests.java
+++ src/test/java/AutomationTests.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class AutomationTests {
+import org.testng.annotations.Test;
+
+public class AutomationTests {
+
+    @Test
+    public void test1() {
+
+    }
+
+    @Test
+    public void test2() {
+
+    }
+
+    @Test
+    public void test3() {
+
+    }
+
+    @Test
+    public void test4() {
+        // Test scenario for Test 4
+    }
+
+    @Test
+    public void test5() {
+        // Test scenario for Test 5
+    }
 }
